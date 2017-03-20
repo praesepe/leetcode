78f5ca57c7c1864d5496f06dbffe5897c9963d79
kychen <kychen@vynca.org>
2017-03-21 03:04:36 +0800

    [leetcode 28] - Added KMP

--- leetcode/028-Implement-strstr/lala_2.cpp
+++ leetcode/028-Implement-strstr/lala_2.cpp
@@ -1,25 +1,53 @@
-#include <stdio.h>
-#include <string.h>
+class Solution {
+private:
+    vector<int> compute_failure_table(string needle) {
+        vector<int> failure(needle.size(), 0);
 
-int main() {
-	const char* str = "ABCAB";
-	int f[10] = {0};
+        int j = 0;
+        for (int i = 1; i< failure.size(); i++) {
+            while (j > 0 && needle[i] != needle[j]) {
+                j = failure[j - 1];
+            }
 
-	int j = 0;
-	for (int i = 1; i < 9; i++) {
-		while (str[j] != str[i] && j > 0) {
-			j = f[j - 1];
+			if (needle[i] == needle[j]) {
+				j++;
+				failure[i] = j;
+			}
 		}
 
-		if (str[j] == str[i]) {
-			f[i] = j + 1;
-			j++;
+        return failure;
+    }
+public:
+    int strStr(string haystack, string needle) {
+        //
+        if (haystack.size() < needle.size()) {
+            return -1;
+        }
+        if (needle.empty()) {
+            return 0;
+        }
+
+		// compute failure table
+		int result = -1;
+		vector<int> failure = this->compute_failure_table(needle);
+
+		// search
+		int j = 0;
+		for (int i = 0; i < haystack.size(); i++) {
+			while (j > 0 && haystack[i] != needle[j]) {
+				j = failure[j - 1];
+			}
+
+            if (haystack[i] == needle[j]) {
+                j++;
+            }
+
+			if (j == needle.size()) {
+				result = i - needle.size() + 1;
+				break;
+			}
 		}
-	}
 
-
-	for (int i = 0; i < 10; i++) {
-		printf("%d\n", f[i]);
-	}
-	return 0;
-}
+        return result;
+    }
+};
