9edc024052e70d0dd68960f0724dd457d93073f3
derek <derekhound@gmail.com>
2017-03-24 11:28:17 +0800

    Add leetcode [167]

---
+++ leetcode/167-Two-Sum-II-Input-array-is-sorted/derek.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& numbers, int target) {
+		int i = 0, j = numbers.size() - 1;
+		while (i < j) {
+			int sum = numbers[i] + numbers[j];
+			if (sum < target) {
+				i++;
+			} else if (sum > target) {
+				j--;
+			} else {
+				return {i+1, j+1};
+			}
+		}
+		throw std::invalid_argument("No soltion");
+	}
+};
