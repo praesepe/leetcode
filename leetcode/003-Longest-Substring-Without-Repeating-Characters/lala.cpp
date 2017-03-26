4d0b8f67e68235ea9afcbf773b2275f813c45ff4
kychen <kychen@vynca.org>
2017-03-27 02:44:03 +0800

    Added leetcode 003

---
+++ leetcode/003-Longest-Substring-Without-Repeating-Characters/lala.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        string::size_type result = 0, right = 0, left = 0;
+        for (left = right; left < s.size(); left++) {
+            unordered_set<char> us;
+            for (right = left; right < s.size(); right++) {
+                if (us.find(s[right]) != us.end()) {
+                    break;
+                } else {
+                    us.insert(s[right]);
+                }
+            }
+			result = max(result, right - left);
+		}
+
+        return result;
+    }
+};
