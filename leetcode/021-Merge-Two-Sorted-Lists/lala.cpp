8178f050670ed961e57dcad496ffa6391b1fc1f0
lalayueh <lalayueh@gmail.com>
2017-04-10 00:09:13 +0800

    Fixed 021 - Use dummy head

--- leetcode/021-Merge-Two-Sorted-Lists/lala.cpp
+++ leetcode/021-Merge-Two-Sorted-Lists/lala.cpp
@@ -10,33 +10,24 @@ struct ListNode {
 
 class Solution {
 public:
-	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-		ListNode* result = NULL, *walker = NULL, *p1 = l1, *p2 = l2;
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode *result, *walker, *dummy, *p1 = l1, *p2 = l2;
 
-		while (p1 != NULL && p2 != NULL) {
-			if (p1->val < p2->val) {
-				if (!result) {
-					walker = result = p1;
-				} else {
-					walker->next = p1;
-					walker = walker->next;
-				}
-				p1 = p1->next;
-			} else {
-				if (!result) {
-					walker = result = p2;
-				} else {
-					walker->next = p2;
-					walker = walker->next;
-				}
-				p2 = p2->next;
-			}
-		}
+        walker = dummy = new ListNode(0);
+        while (p1 != NULL && p2 != NULL) {
+            if (p1->val < p2->val) {
+                walker->next = p1;
+                walker = walker->next;
+                p1 = p1->next;
+            } else {
+                walker->next = p2;
+                walker = walker->next;
+                p2 = p2->next;
+            }
+        }
+        walker->next = p1 ? p1 : p2;
 
-		if (result) {
-			walker->next = p1 ? p1 : p2;
-		}
-
-		return result ? result : l1 ? l1 : l2;
-	}
+        result = dummy->next;
+        return result? result : l1 ? l1 : l2;
+    }
 };
