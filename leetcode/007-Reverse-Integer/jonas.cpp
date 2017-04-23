4a9c84d963217afa0cf91f9ef05b43a96963af83
jonash <jonash@synology.com>
2017-04-23 20:53:56 +0800

    Add leetcode 007

--- leetcode/007-Reverse-Integer/jonas.cpp
+++ leetcode/007-Reverse-Integer/jonas.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int reverse(int x) {
+		long long int next = 0;
 		int result = 0;
-		int mod, next;
+		int mod;
 
 		while (x != 0) {
 			mod = x % 10;
-			next = result * 10 + mod;
+			next = (long long)result * 10 + mod;
 
-			// Ensure it doesn't overflow
-			if ( (next - mod) / 10 != result) {
+			if (next > INT_MAX || next < INT_MIN) {
 				return 0;
 			}
+
 			x = x / 10;
 			result = next;
 		}
