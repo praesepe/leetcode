5a84e9739925f5b30b2dc577088cd38e3aafd153
lalayueh <lalayueh@gmail.com>
2017-04-30 18:05:28 +0800

    Refine leetcode 066

--- leetcode/066-Plus-One/lala.cpp
+++ leetcode/066-Plus-One/lala.cpp
@@ -1,32 +1,22 @@
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
-	vector<int> plusOne(vector<int>& digits) {
-		if (digits.empty()) {
-			return {1};
-		}
+    vector<int> plusOne(vector<int>& digits) {
+        int carry = 1;
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            int sum = digits[i] + carry;
+            digits[i] = sum % 10;
+            carry = sum / 10;
+        }
 
-		vector<int> result(digits.size());
-		result[0] = 1;
-		for (int i = digits.size() - 1; i >= 0; i--) {
-			int resultIndex = digits.size() - 1 - i;
-			int num = digits[i] + result[resultIndex];
-			result[resultIndex] = (digits[i] + result[resultIndex]) % 10;
+        if (carry == 1) {
+            vector<int> newDigits(digits.size() + 1, 0);
+            newDigits[0] = 1;
+            return newDigits;
+        }
 
-			num /= 10;
-			for (int j = 1; num != 0; j++) {
-				if (resultIndex + j >= result.size()) {
-					result.resize(resultIndex + j + 1, 0);
-				}
-				result[resultIndex + j] += num % 10;
-				num /= 10;
-			}
-		}
-
-		reverse(result.begin(), result.end());
-		return result;
-	}
+        return digits;
+    }
 };
