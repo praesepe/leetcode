6792286b187dbdf33f29330c975d3c33ddceaed4
lalayueh <lalayueh@gmail.com>
2017-04-30 18:36:58 +0800

    Optimize 009

--- leetcode/009-Palindrome-Number/lala.cpp
+++ leetcode/009-Palindrome-Number/lala.cpp
@@ -1,19 +1,35 @@
 #include <cmath>
-
+#define MAPPING_SIZE 10
 using namespace std;
 
 class Solution {
 private:
 	int loopCount(int x) {
-		int count = 0;
-		for (int i = 0; x != 0; i++) {
-			x /= 10;
-			count++;
+		int left = 0;
+		int right = MAPPING_SIZE - 1;
+
+		while (right > left) {
+			int middle = (right + left) / 2;
+			if (this->power_mapping[middle] > x) {
+				right = middle - 1;
+			} else if (this->power_mapping[middle] < x) {
+				left = middle + 1;
+			} else {
+				return middle;
+			}
 		}
 
-		return count;
+		return x / this->power_mapping[left] > 0 ? left : left - 1;
 	}
 public:
+	Solution() {
+		int power = 1;
+		for (size_t i = 0; i < MAPPING_SIZE; i++) {
+			power_mapping[i] = power;
+			power = power * 10;
+		}
+	}
+
 	bool isPalindrome(int x) {
 		if (x < 0) {
 			return false;
@@ -23,9 +39,9 @@ public:
 		int count = this->loopCount(x);
 
 		// main
-		for (int i = 0; i < count / 2; i++) {
-			int head = (int) (x / pow(10, count - i - 1)) % 10;
-			int tail = (int) (x / pow(10, i)) % 10;
+		for (int i = 0; i < (count + 1 / 2); i++) {
+			int head = (int) (x / this->power_mapping[count - i]) % 10;
+			int tail = (int) (x / this->power_mapping[i]) % 10;
 			if (head != tail) {
 				return false;
 			}
@@ -33,4 +49,6 @@ public:
 
 		return true;
 	}
+private:
+	int power_mapping[MAPPING_SIZE];
 };
