3333d90555177977e1bbe6bc31a2f190760ee3be
lalayueh <lalayueh@gmail.com>
2017-05-19 23:26:48 +0800

    Added 164 pingeonhole principle

---
+++ leetcode/164-Maximum-Gap/lala_pigeonhole.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+struct Bucket {
+	int min;
+	int max;
+	bool is_empty;
+	Bucket() : min(INT_MAX), max(INT_MIN), is_empty(true) {};
+};
+
+class Solution {
+public:
+	int maximumGap(vector<int>& nums) {
+		if (nums.size() < 2) return 0;
+
+		int maxValue = *max_element(nums.begin(), nums.end());
+		int minValue = *min_element(nums.begin(), nums.end());
+		int interval = max(1, int(((maxValue - minValue) / (nums.size() - 1))));
+
+		// setup buckets
+		vector<Bucket> buckets((maxValue - minValue) / interval + 1) ;
+		for (const auto num : nums) {
+			if (num == minValue || num == maxValue) continue;
+
+			int index = (num - minValue) / interval;
+			buckets[index].is_empty = false;
+			buckets[index].min = min(buckets[index].min, num);
+			buckets[index].max = max(buckets[index].max, num);
+		}
+
+		// run
+		int result = 0;
+		int previousMax = minValue;
+		for (const auto bucket : buckets) {
+			if (bucket.is_empty) continue;
+
+			result = max(result, bucket.min - previousMax);
+			previousMax = bucket.max;
+		}
+
+		return max(result, maxValue - previousMax);
+	}
+};
