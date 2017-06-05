168066178e13aa4224d1d827c11357be6c1c2cfb
SillyHP <yihsun817@gmail.com>
2017-06-05 22:25:26 +0800

    Modify 012.cpp

--- leetcode/012-Integer-to-Roman/phyllis.cpp
+++ leetcode/012-Integer-to-Roman/phyllis.cpp
@@ -1,38 +1,54 @@
-
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
 public:
-    int isBalanced(TreeNode* root){
-        int lDepth, rDepth;
-        lDepth = rDepth = -1;
-        return isBalanced(root, &lDepth);
-    }
+    string intToRoman(int num) {
+        char cstr[16] = {'\0'};
+        char sign[13] ={'C','D','M', 'X', 'L', 'C', 'I', 'V', 'X'};
+        int i = 0;
+        int divide =1000;
+        int sign_ptr = 0;
+        
+        // handle thousands of digits，num = 1000~4000
+        if(num >= 1000){
+            int total = num / 1000;
+            while(i < total){
+                cstr[i++] = 'M';
+            }
+        }
+        
+        while(divide >= 10){
+            num = num % divide;
+            divide = divide /10;
+            
+            // e.g. num = 900~999 
+            if(num >= 9*divide){
+                cstr[i++] = sign[sign_ptr];
+                cstr[i++] = sign[sign_ptr+2];
+            // e.g. num = 500~899    
+            } else if(num >= 5*divide){
+                cstr[i++] = sign[sign_ptr+1];
+                int append = (num-5*divide)/divide;
+                while(append-- > 0){
+                    cstr[i++] = sign[sign_ptr];
+                }
+            // e.g. num = 400~499      
+            } else if(num >= 4*divide){
+                cstr[i++] = sign[sign_ptr];
+                cstr[i++] = sign[sign_ptr+1];
+            // e.g. num = 100~399
+            } else if(num >= divide){
+                int total = num/divide;
+                while(total-- > 0){
+                    cstr[i++] = sign[sign_ptr];;
+                }
+            }
+            sign_ptr = sign_ptr + 3;
+        }
     
-    bool isBalanced(TreeNode* root, int *depth) {
-        if(!root) return true;
-
-        int lDepth, rDepth;
-        lDepth = rDepth = -1;
-        bool rBalanced, lBalanced;
-        if(root->right != NULL) rBalanced = isBalanced(root->right, &rDepth);
-        else rBalanced =true;
-        
-        if(root->left != NULL) lBalanced = isBalanced(root->left, &lDepth);
-        else lBalanced =true;
-        
-        if(lBalanced && rBalanced){ 
-           *depth = (rDepth > lDepth)? rDepth : lDepth;
-           *depth = *depth + 1;
-           return (abs(rDepth-lDepth) <= 1) ? true : false;
-          
-        } else return false;
+        std::string str(cstr);
+    
+        return str;
     }
 };
+
+
+
