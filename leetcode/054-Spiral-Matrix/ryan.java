20764beb223f36c7e1f3ff796f21281b692aab5d
a326ryan <a326ryan@gmail.com>
2017-06-11 20:54:10 +0800

    Add leetcode 054

---
+++ leetcode/054-Spiral-Matrix/ryan.java
@@ -0,0 +1,47 @@
+public class Solution {
+    
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList();
+        if (matrix.length == 0) {
+            return res;
+        }
+
+        int rowBegin = 0;
+        int rowEnd = matrix.length - 1;
+        int colBegin = 0;
+        int colEnd = matrix[0].length - 1;
+
+        while (rowBegin <= rowEnd && colBegin <= colEnd) {
+            // left to right
+            for (int i = colBegin; i <= colEnd; i++) {
+                res.add(matrix[rowBegin][i]);
+            }
+            rowBegin++;
+
+            // top to down
+            for (int i = rowBegin; i <= rowEnd; i++) {
+                res.add(matrix[i][colEnd]);
+            }
+            colEnd--;
+
+            // right to left
+            if (rowBegin <= rowEnd) {
+                for (int i = colEnd; i >= colBegin; i--) {
+                    res.add(matrix[rowEnd][i]);
+                }
+            }
+            rowEnd--;
+
+            // down to top
+            if (colBegin <= colEnd) {
+                for (int i = rowEnd; i >= rowBegin; i--) {
+                    res.add(matrix[i][colBegin]);
+                }
+            }
+            colBegin++;
+        }
+
+        return res;
+    }
+    
+}
