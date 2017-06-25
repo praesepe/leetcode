9a4c323ed46c3bb67b94aaf0edc93879bc6fdac0
lalayueh <lalayueh@gmail.com>
2017-06-25 17:49:30 +0800

    Refined 070

--- leetcode/070-Climbing-Stairs/lala.cpp
+++ leetcode/070-Climbing-Stairs/lala.cpp
@@ -1,32 +1,73 @@
 #include <stdio.h>
+#include <unordered_map>
 
-#define OFFSTET 2
+using namespace std;
 
 class Solution {
-public:
-	int climbStairs(int n) {
-		/*
-		   int goal = n + OFFSTET;
-		   int* stairs = new int[goal + 1];
+private:
+	int solve_recursive(int n) {
+		if (n == 0 || n == 1) return 1;
 
-		   stairs[0] = 0;
-		   stairs[1] = 0;
-		   stairs[2] = 1;
-		   for (int i = OFFSTET + 1; i <= goal ; i++) {
-		   stairs[i] = stairs[i - 1] + stairs[i - 2];
-		   }
+		return solve_recursive(n - 1) + solve_recursive(n - 2);
+	}
 
-		   delete [] stairs;
-		   return stairs[goal];
-		   */
-		int tn2 = 1, tn1 = 1;
+	int recursive_with_memorization(int n, unordered_map<int, int>& cache) {
+		// check cache
+		auto answer = cache.find(n);
+		if (answer != cache.end()) return answer->second;
 
+		// compute result
+		if (n == 0 || n == 1) return 1;
+		int result = recursive_with_memorization(n - 1, cache) + recursive_with_memorization(n - 2, cache);
+
+		// set cache
+		cache[n] = result;
+
+		return result;
+	}
+
+	int solve_recursive_with_memorization(int n) {
+		unordered_map<int, int> cache;
+
+		return recursive_with_memorization(n, cache);
+	}
+
+	int solve_dp(int n) {
+		int* stairs = new int[n + 1];
+
+		stairs[0] = 1;
+		stairs[1] = 1;
 		for (int i = 2; i <= n; i++) {
-			int temp = tn1;
-			tn1 += tn2;
-			tn2 = temp;
+			stairs[i] = stairs[i - 1] + stairs[i - 2];
 		}
 
-		return tn1;
+		int result = stairs[n];
+		delete [] stairs;
+
+		return result;
+	}
+
+	int solve_dp_spaze_optimization(int n) {
+		int result = 1, tn1 = 1, tn2 = 1;
+
+		for (int i = 2; i <= n; i++) {
+			result = tn1 + tn2;
+			tn2 = tn1;
+			tn1 = result;
+		}
+
+		return result;
+	}
+
+	int solve_math(int n) {
+		int np = n + 1;
+		double phi = (1 + sqrt(5)) * 0.5;
+		double fib = (pow(phi, np) - pow(1 - phi, np)) / sqrt(5);
+
+		return round(fib);
+	}
+public:
+	int climbStairs(int n) {
+		return this->solve_dp_spaze_optimization(n);
 	}
 };
