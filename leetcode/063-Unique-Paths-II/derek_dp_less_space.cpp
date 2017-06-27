78b3abbd8af1d482c014f7e51223313ea858954c
derek <derekhound@gmail.com>
2017-06-28 05:57:14 +0800

    Add leetcode 063

---
+++ leetcode/063-Unique-Paths-II/derek_dp_less_space.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+
+        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+        vector<int> dp(n, 0);
+        dp[0] = 1;
+
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (obstacleGrid[r][c] == 1) {
+                    dp[c] = 0;
+                } else if (c > 0) {
+                    dp[c] += dp[c-1];
+                }
+            }
+        }
+
+        return dp[n-1];
+    }
+};
