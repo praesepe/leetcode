650869ac09912bff5956cf2968c5fe94a7afc73a
derek <derekhound@gmail.com>
2017-07-09 07:11:29 +0800

    Add 153

---
+++ leetcode/153-Find-Minimum-in-Rotated-Sorted-Array/derek.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left < right) {
+            if (nums[left] < nums[right]) {
+                break;
+            }
+
+            int middle = (left + right) / 2;
+            if (nums[left] <= nums[middle]) {
+                left = middle + 1;
+            } else {
+                right = middle;
+            }
+        }
+
+        return nums[left];
+    }
+};
