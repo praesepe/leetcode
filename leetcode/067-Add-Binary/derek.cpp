20a04718f8b23a0eafcb092230e5432b5be57073
derek <derekhound@gmail.com>
2017-07-18 10:59:45 +0800

    Add 067

--- leetcode/067-Add-Binary/derek.cpp
+++ leetcode/067-Add-Binary/derek.cpp
@@ -1,57 +1,27 @@
 class Solution {
 public:
 	string addBinary(string a, string b) {
-		if (a.length() < b.length()) {
-			return addBinary(b, a);
-		}
+		int n = max(a.length(), b.length());
 
-		reverse(a.begin(), a.end());
-		reverse(b.begin(), b.end());
+		// fill leading zero
+		a = string(n - a.length(), '0') + a;
+		b = string(n - b.length(), '0') + b;
 
-		for (int i = b.length(); i < a.length(); i++) {
-			b += '0';
-		}
-
+		string ans;
 		int carry = 0;
-		string result;
-
-		for (int i = 0; i < a.length(); i++) {
-			if (carry == 0) {
-				if (a[i] == '0' && b[i] == '0') {
-					result += '0';
-					carry = 0;
-				} else if (a[i] == '0' && b[i] == '1') {
-					result += '1';
-					carry = 0;
-				} else if (a[i] == '1' && b[i] == '0') {
-					result += '1';
-					carry = 0;
-				} else {
-					result += '0';
-					carry = 1;
-				}
-			} else {
-				if (a[i] == '0' && b[i] == '0') {
-					result += '1';
-					carry = 0;
-				} else if (a[i] == '0' && b[i] == '1') {
-					result += '0';
-					carry = 1;
-				} else if (a[i] == '1' && b[i] == '0') {
-					result += '0';
-					carry = 1;
-				} else {
-					result += '1';
-					carry = 1;
-				}
-			}
+		for (int i = n - 1; i >= 0; i--) {
+			// add two character with carry
+			carry = carry + (a[i] - '0') + (b[i] - '0');
+			// calculate remainder
+			ans = (char)(carry % 2 + '0') + ans;
+			// calculate quotient
+			carry /= 2;
 		}
 
 		if (carry == 1) {
-			result += '1';
+			ans = '1' + ans;
 		}
 
-		reverse(result.begin(), result.end());
-		return result;
+		return ans;
 	}
 };
