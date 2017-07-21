c50f69b3c95ce93abe1120569acb2fae611d76e1
lalayueh <lalayueh@gmail.com>
2017-07-21 20:19:37 +0800

    Added leetcode 091

---
+++ leetcode/091-Decode-Ways/lala.cpp
@@ -0,0 +1,29 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int numDecodings(string s) {
+        if (s.size() == 0) return 0;
+        if (s.size() == 1) return s[0] == '0' ? 0 : 1;
+        /*
+         *  ways_2 ways_1
+         *  ....   last   current
+         */
+        int result;
+        int ways_2 = 1, ways_1 = s[0] == '0' ? 0 : 1;
+        char last = s[0];
+        for (size_t i = 1; i < s.size(); i++) {
+            int current = s[i] - '0';
+            int num = stoi(s.substr(i - 1, 2));
+
+            result = (current != 0 ? ways_1 : 0) + ((num >= 10 && num <= 26) ? ways_2 : 0);
+
+            ways_2 = ways_1;
+            ways_1 = result;
+            last = current;
+        }
+        return result;
+    }
+};
