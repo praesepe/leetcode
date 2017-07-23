60f35adf5a7897f1ee7ac97474a6808084808d51
lalayueh <lalayueh@gmail.com>
2017-07-23 14:50:19 +0800

    164 Generalized bit based radix sort

--- leetcode/164-Maximum-Gap/lala_radix_sort_optimize.cpp
+++ leetcode/164-Maximum-Gap/lala_radix_sort_optimize.cpp
@@ -4,38 +4,48 @@
 #include <vector>
 
 using namespace std;
-
 #define DEGREE 32
-#define BUCKET_SIZE 2
+// RADIX BASE: 2^BITS_BASE
+#define BITS_BASE 3
+#define MASK ((0x1LL << BITS_BASE) - 1)
+#define BUCKET_SIZE (1 << BITS_BASE)
+
 
 class Solution {
 private:
 	int get_loop_count(const vector<int>& nums) {
 		int max_num = *max_element(nums.begin(), nums.end());
 		int loop_count;
-		for (loop_count = 1; loop_count < DEGREE; loop_count++) {
-			if (0 == (max_num >> loop_count)) break;
+		int shift = 1;
+		for (loop_count = 1; loop_count < DEGREE / BITS_BASE; loop_count++) {
+			if (0 == (max_num >> shift)) break;
+			shift += BITS_BASE;
 		}
 
 		return loop_count;
 	}
 
-	void counting(int* count, const vector<int>& nums, int digit_index) {
+	void counting(int* count, const vector<int>& nums, int shift) {
 		memset(count, 0, sizeof(*count) * BUCKET_SIZE);
 		for (const auto num : nums) {
-			int digit = (num >> digit_index) & 0x1;
+			int digit = (num >> shift) & MASK;
 			count[digit]++;
 		}
 		// accumulation
-		count[1] += count[0];
+		int accumulation = count[0];
+		for (int i = 1; i < BUCKET_SIZE; i++) {
+			count[i] += accumulation;
+			accumulation = count[i];
+		}
 	}
 
-	void rolling(const vector<int>& src, vector<int>& dest, int count[], int digit_index) {
+	void rolling(const vector<int>& src, vector<int>& dest, int count[], int shift) {
 		for (int i = src.size() - 1; i >= 0; i--) {
 			int num = src[i];
-			int digit = (num >> digit_index) & 0x1;
+			int digit = (num >> shift) & MASK;
 			int index = count[digit] - 1;
 
+			if (index == -1) continue;
 			dest[index] = num;
 			count[digit]--;
 		}
@@ -57,13 +67,13 @@ public:
 		for (int i = 0; i < loop_count; i++) {
 			vector<int> &src = runs[i % 2];
 			vector<int> &dest = runs[(i + 1) % 2];
+			int shift = i * BITS_BASE;
 
 			// counting
-			this->counting(count, nums, i);
+			this->counting(count, nums, shift);
 
 			// rolling
-			this->rolling(src, dest, count, i);
-
+			this->rolling(src, dest, count, shift);
 		}
 
 		// compute result
