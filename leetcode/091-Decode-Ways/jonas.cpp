71d93acfb8e9976805dccf89bf57a10d27f21402
Jonas <pingchehsiao@gmail.com>
2017-08-13 17:31:49 +0800

    Add comment on leetcode 091

--- leetcode/091-Decode-Ways/jonas.cpp
+++ leetcode/091-Decode-Ways/jonas.cpp
@@ -1,22 +1,23 @@
 class Solution {
  public:
     int numDecodings(std::string s) {
-		std::vector<int> fib(s.size());
+		std::vector<int> fib(s.size()+1);
 
 		if (s.empty() || s[0] == '0') {
 			return 0;
 		}
 
 		fib[0] = 1;
-		fib[1] = s[1] != '0' ? 1 : 0;
-		fib[1] += stoi(s.substr(0, 2)) <= 26 ? 1 : 0;
+		fib[1] = 1;
 
-		for (int i = 2; i < s.size(); i++) {
-			fib[i] = s[i] != '0' ? fib[i-1] : 0;
-			fib[i] += stoi(s.substr(i - 1, 2)) <= 26 && s[i - 1] != '0'
-						  ? fib[i - 2]
-						  : 0;
+		for (int i = 2; i <= s.size(); i++) {
+			// walk one step, prevent 0
+			fib[i] = s[i-1] != '0' ? fib[i-1] : 0;
+
+			// walk two steps, prevent cases like 02(<10) or 34(>26)
+			int n = stoi(s.substr(i - 2, 2));
+			fib[i] += n >= 10 && n <= 26 ? fib[i - 2] : 0;
 		}
-		return fib[s.size()-1];
+		return fib[s.size()];
     }
 };
