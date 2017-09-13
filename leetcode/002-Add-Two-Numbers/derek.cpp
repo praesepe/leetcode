ae95b3aadfd12ed95baa4fc204fb74599369aa78
derek <derekhound@gmail.com>
2017-09-14 06:03:55 +0800

    Add 002

--- leetcode/002-Add-Two-Numbers/derek.cpp
+++ leetcode/002-Add-Two-Numbers/derek.cpp
@@ -1,31 +1,31 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode dummy(0);
-        ListNode *curr = &dummy;
-        int carry = 0;
+        ListNode* p = &dummy;
 
-        while (l1 != NULL || l2 != NULL) {
+        int carry = 0, sum;
+        while (l1 != NULL || l2!= NULL) {
             int x = l1 ? l1->val : 0;
             int y = l2 ? l2->val : 0;
-            int digit = carry + x + y;
-            carry = digit / 10;
-            curr->next = new ListNode(digit % 10);
-            curr = curr->next;
-            if (l1) l1 = l1->next;
-            if (l2) l2 = l2->next;
+            sum = x + y + carry;
+            carry = sum / 10;
+            sum = sum % 10;
+
+            p->next = new ListNode(sum);
+            p = p->next;
+
+            if (l1) {
+                l1 = l1->next;
+            }
+            if (l2) {
+                l2 = l2->next;
+            }
         }
 
         if (carry > 0) {
-            curr->next = new ListNode(carry);
+            p->next = new ListNode(carry);
+            p = p->next;
         }
 
         return dummy.next;
