e1dc6a2b12ac84420e603a6a78b832d59325ef73
Tina Chang <welcomepig@gmail.com>
2017-09-23 16:55:52 +0800

    Updated 438

--- leetcode/438-Find-All-Anagrams-in-a-String/tina.cpp
+++ leetcode/438-Find-All-Anagrams-in-a-String/tina.cpp
@@ -5,33 +5,36 @@ public:
         int slen = s.length();
         int plen = p.length();
         
-        if (slen < plen) return result;
+        if (slen == 0 && plen == 0 && slen < plen) return result;
         
-        vector<int> primes = {
-            2, 3, 5, 7, 11, 13,
-            17, 19, 23, 29, 31,
-            37, 41, 43, 47, 53, 
-            59, 61, 67, 71, 73,
-            79, 83, 89, 91, 97
-        };
+        int i, j, count;
+        unordered_map<char, int> smap;
         
-        int i, j, tmp, sum;
-        
-        sum = 0;
         for (i = 0;i < plen; i++) {
-            sum += primes[p[i] - 'a'];
+            smap[p[i]]++;
         }
         
-        tmp = 0;
-        for (i = 0;i < plen; i++) {
-            tmp += primes[s[i] - 'a'];
-        }
-        if (sum == tmp) result.push_back(0);
+        i = j = 0;
+        count = plen;
         
-        for (j = 1;j <= slen - plen; j++) {
-            tmp -= primes[s[j - 1] - 'a'];
-            tmp += primes[s[j + plen - 1] - 'a'];
-            if (sum == tmp) result.push_back(j);
+        while (j < slen) {
+            if (smap[s[j]] > 0) {
+                count--;
+            }
+            smap[s[j]]--;
+            j++;
+            
+            if (count == 0) {
+                result.push_back(i);
+            }
+            
+            if (j - i == plen) {
+                if (smap[s[i]] >= 0) {
+                    count++;
+                }
+                smap[s[i]]++;
+                i++;
+            }
         }
         
         return result;
