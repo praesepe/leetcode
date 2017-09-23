d6e56530bcda6e6c5041d59c6cdb9381091b3c9f
lalayueh <lalayueh@gmail.com>
2017-09-23 20:23:34 +0800

    Refined 242

--- leetcode/242-Valid-Anagram/lala.cpp
+++ leetcode/242-Valid-Anagram/lala.cpp
@@ -5,15 +5,12 @@ using namespace std;
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        int dummy[26] = {0};
         int table[26] = {0};
 
         for (const auto c : t) table[c - 'a']++;
         for (const auto c : s) table[c - 'a']--;
 
-        for (size_t i = 0; i < 26; i++) {
-            if (table[i] != 0) return false;
-        }
-
-        return true;
+        return memcmp(dummy, table, sizeof(dummy)) == 0;
     }
 };
