e26d980e4816bed96435011b3c54d32cd52f6919
lalayueh <lalayueh@gmail.com>
2017-10-01 14:49:04 +0800

    Added 139 comments

--- leetcode/139-Word-Break/lala.cpp
+++ leetcode/139-Word-Break/lala.cpp
@@ -1,29 +1,42 @@
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 #include <string>
 
 using namespace std;
-
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_multimap<char, string> dict;
+        unordered_map<char, unordered_set<string>> dict;
 
         // for speedup
         for (const auto word : wordDict) {
-            dict.insert(make_pair(word[0], word));
+            char key = word[0];
+            auto itr = dict.find(key);
+            if (itr == dict.end()) {
+                dict[key] = {word};
+            } else {
+                itr->second.insert(word);
+            }
         }
 
+
+        // init table
         bool success[s.size() + 1];
         memset(success, 0, sizeof(success));
-
         success[0] = true;
+
+        // F(i) = F(i - 1) && S[i..|Wj|] == Wj
         for (size_t i = 0; i < s.size(); i++) {
             if (!success[i]) continue;
 
-            auto range = dict.equal_range(s[i]);
-            for (auto itr = range.first; itr != range.second; ++itr) {
-                string word = itr->second;
+            // find set
+            auto itr = dict.find(s[i]);
+            if (itr == dict.end()) continue;
+
+            // dp
+            auto word_set = itr->second;
+            for (auto word : word_set) {
                 if (s.substr(i, word.size()) == word) {
                     success[i + word.size()] = true;
                 }
