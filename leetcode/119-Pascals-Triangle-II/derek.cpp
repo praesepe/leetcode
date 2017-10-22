e0c4da9562f6035fa3fdb933f16aa931fa5eae42
lalayueh <lalayueh@gmail.com>
2017-10-22 21:38:38 +0800

    Revert "Added 119"
    This reverts commit dd930cf9bfb8ba244eb685e33d30e9fabdd16b62.

--- leetcode/119-Pascals-Triangle-II/derek.cpp
+++ leetcode/119-Pascals-Triangle-II/derek.cpp
@@ -1,17 +1,15 @@
-#include <vector>
-
-using namespace std;
-
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        vector<int> result(rowIndex + 1, 1);
-        // loop count
-        for (int i = 2; i <= rowIndex; i++) {
-            for (int j = i - 1; j >= 1; j--) {
+        vector<int> result;
+
+        for (int i = 0; i <= rowIndex; i++) {
+            result.push_back(1);
+            for (int j = result.size() - 2; j >= 1; j--) {
                 result[j] = result[j-1] + result[j];
             }
         }
+
         return result;
     }
 };
